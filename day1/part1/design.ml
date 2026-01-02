38bf61912d8058ad7bc774409e337805b1ed7358
padhmanethrri <padhmanethrrisuresh26@gmail.com>
2026-01-02 20:37:55 +0530

    Document role of design file

--- day1/part1/design.ml
+++ day1/part1/design.ml
@@ -1,1 +1,6 @@
-(* Design logic for Day 1 Part 1 will live here *)
+(*
+  Design logic for Day 1 Part 1.
+  This file is intended to contain the core computation logic only.
+  It is written in a way that can later be mapped to hardware-style
+  dataflow without relying on file I/O or printing.
+  *)
